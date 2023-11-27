9d29e6afcb76eec49f16217fd9f8e41ee5563f5f
atikkhannilgar <63293826+atikkhannilgar@users.noreply.github.com>
2023-11-27 12:49:36 +0100

    Update new app.js

--- apps/AccelRecorder/app.js
+++ apps/AccelRecorder/app.js
@@ -1,117 +1,71 @@
 var fileNumber = 0;
 var MAXLOGS = 9;
 var logRawData = false;
-var recording = false; // New variable to track recording state
+var recording = false;
 var recordingInterval;
 var recordingWidget;
 
 function createRecordingWidget() {
   recordingWidget = {
-    width: 12, // Adjust the width as needed
+    width: 12,
     draw: function() {
       g.reset();
-      g.drawImage(recording ? require("heatshrink").decompress(atob("i0WwH+H4fz4A/AiD/AjD/AiH+AiH+AjD/AjD/AjD/AiD/")) : require("heatshrink").decompress(atob("i0WwH+H4fz4A/AiD/AjD/AiH+AiH+AjD/AjD/AjD/AiD/")), this.x, this.y);
+      g.drawImage(
+        recording
+          ? require("heatshrink").decompress(atob("i0WwH+H4fz4A/AiD/AjD/AiH+AiH+AjD/AjD/AjD/AiD/"))
+          : require("heatshrink").decompress(atob("i0WwH+H4fz4A/AiD/AjD/AiH+AiH+AjD/AjD/AjD/AiD/")),
+        this.x,
+        this.y
+      );
     }
   };
   WIDGETS["recording"] = recordingWidget;
 }
 
-// Declare accelHandler in the global scope
 function accelHandler(accel) {
-  // ... (unchanged code for handling accelerometer data)
-}
+  if (recording) {
+    var t = getTime() - start;
 
-function getFileName(n) {
-  return "accellog."+n+".csv";
-}
-
-function showMenu() {
-  var menu = {
-    "" : { title : "Accel Logger" },
-    /*LANG*/"< Back": () => Bangle.showLauncher(),
-    "File No" : {
-      value : fileNumber,
-      min : 0,
-      max : MAXLOGS,
-      onchange : v => { fileNumber=v; }
-    },
-    "Start" : function() {
-      toggleRecord();
-    },
-    "View Logs" : function() {
-      viewLogs();
-    },
-    "Log raw data" : {
-      value : !!logRawData,
-      onchange : v => { logRawData=v; }
-    },
-    "Stop" : function() {
-      stopRecord();
-    },
-  };
-  E.showMenu(menu);
-}
-
-function stopRecord() {
-    recording = false;
-    Bangle.removeListener('accel', accelHandler);
-    clearInterval(recordingInterval);
-    E.showMessage("stopping...");
-    showMenu(); // Go back to the main menu after stopping recording
-    if (recordingWidget) {
-      delete WIDGETS["recording"];
-      recordingWidget = undefined;
+    // Write accelerometer data to file
+    if (logRawData) {
+      f.write([
+        t * 1000,
+        accel.x * 8192,
+        accel.y * 8192,
+        accel.z * 8192,
+        accel.mag * 8192
+      ]
+        .map(n => Math.round(n))
+        .join(",") + "\n");
+    } else {
+      f.write([
+        Math.round(t * 1000),
+        accel.x,
+        accel.y,
+        accel.z,
+        accel.mag
+      ].join(",") + "\n");
     }
-}
 
-function viewLog(n) {
-  E.showMessage("Loading...");
-  var f = require("Storage").open(getFileName(n), "r");
-  var records = 0, l = "", ll="";
-  while ((l=f.readLine())!==undefined) {records++;ll=l;}
-  var length = 0;
-  if (ll) length = Math.round( (ll.split(",")[0]|0)/1000 );
-
-  var menu = {
-    "" : { title : "Log "+n },
-    "< Back" : () => { viewLogs(); },
-    "Back to Menu" : () => { showMenu(); },
-  };
-  menu[records+" Records"] = "";
-  menu[length+" Seconds"] = "";
-  menu["DELETE"] = function() {
-    E.showPrompt("Delete Log "+n).then(ok=>{
-      if (ok) {
-        E.showMessage("Erasing...");
-        var f = require("Storage").open(getFileName(n), "r");
-        f.erase();
-        viewLogs();
-      } else viewLog(n);
-    });
-  };
-
-  E.showMenu(menu);
-}
-
-function viewLogs() {
-  var menu = {
-    "" : { title : "Logs" },
-    "< Back" : () => { showMenu(); }
-  };
-
-  var hadLogs = false;
-  for (var i=0;i<=MAXLOGS;i++) {
-    var f = require("Storage").open(getFileName(i), "r");
-    if (f.readLine()!==undefined) {
-      (function(i) {
-        menu["Log "+i] = () => viewLog(i);
-      })(i);
-      hadLogs = true;
-    }
+    sampleCount++;
   }
-  if (!hadLogs)
-    menu["No Logs Found"] = function(){};
-  E.showMenu(menu);
+}
+
+function startRecord(force) {
+  createRecordingWidget();
+  if (recording && !force) return;
+
+  var start = getTime();
+  var sampleCount = 0;
+  var f = require("Storage").open(getFileName(fileNumber), "a");
+
+  Bangle.setPollInterval(1000);
+  Bangle.on('accel', accelHandler);
+  recording = true;
+
+  recordingInterval = setInterval(() => {
+    // Do nothing, just keep the recording going
+  }, 1000);
 }
 
 function toggleRecord() {
@@ -124,55 +78,111 @@ function toggleRecord() {
   }
 }
 
-function startRecord(force) {
-  createRecordingWidget();
-  if (recording && !force) return; // Avoid starting a new recording while one is already in progress
-
-  // Set up recording logic
-  var start = getTime();
-  var sampleCount = 0;
-
-  // Open the file in write mode ("w") to clear existing content
-  var f = require("Storage").open(getFileName(fileNumber), "w");
-  f.write("Time (ms),X,Y,Z,Total\n");
-
-
-  function accelHandler(accel) {
-    if (recording) {
-      var t = getTime() - start;
-
-      // Write accelerometer data to file
-      if (logRawData) {
-        f.write([
-          t * 1000,
-          accel.x * 8192,
-          accel.y * 8192,
-          accel.z * 8192,
-          accel.mag * 8192,
-        ].map(n => Math.round(n)).join(",") + "\n");
-      } else {
-        f.write([
-          Math.round(t * 1000),
-          accel.x,
-          accel.y,
-          accel.z,
-          accel.mag,
-        ].join(",") + "\n");
+function showMenu() {
+  var menu = {
+    "": { title: "Accel Logger" },
+    "< Back": () => Bangle.showLauncher(),
+    "File No": {
+      value: fileNumber,
+      min: 0,
+      max: MAXLOGS,
+      onchange: v => {
+        fileNumber = v;
       }
+    },
+    "Start": function() {
+      toggleRecord();
+    },
+    "View Logs": function() {
+      viewLogs();
+    },
+    "Log raw data": {
+      value: !!logRawData,
+      onchange: v => {
+        logRawData = v;
+      }
+    },
+    "Stop": function() {
+      stopRecord();
+    }
+  };
+  E.showMenu(menu);
+}
 
-      sampleCount++;
+function stopRecord() {
+  recording = false;
+  Bangle.removeListener('accel', accelHandler);
+  clearInterval(recordingInterval);
+  E.showMessage("stopping...");
+  showMenu();
+  if (recordingWidget) {
+    delete WIDGETS["recording"];
+    recordingWidget = undefined;
+  }
+}
+
+function viewLogs() {
+  var menu = {
+    "": { title: "Logs" },
+    "< Back": () => {
+      showMenu();
+    }
+  };
+
+  var hadLogs = false;
+  for (var i = 0; i <= MAXLOGS; i++) {
+    var f = require("Storage").open(getFileName(i), "r");
+    if (f.readLine() !== undefined) {
+      (function(i) {
+        menu["Log " + i] = () => viewLog(i);
+      })(i);
+      hadLogs = true;
     }
   }
+  if (!hadLogs) menu["No Logs Found"] = function() {};
+  E.showMenu(menu);
+}
 
+function viewLog(n) {
+  E.showMessage("Loading...");
+  var f = require("Storage").open(getFileName(n), "r");
+  var records = 0,
+    l = "",
+    ll = "";
+  while ((l = f.readLine()) !== undefined) {
+    records++;
+    ll = l;
+  }
+  var length = 0;
+  if (ll) length = Math.round((ll.split(",")[0] | 0) / 1000);
 
-  Bangle.setPollInterval(1000);
-  Bangle.on('accel', accelHandler);
-  recording = true; // Update recording state
+  var menu = {
+    "": { title: "Log " + n },
+    "< Back": () => {
+      viewLogs();
+    },
+    "Back to Menu": () => {
+      showMenu();
+    }
+  };
+  menu[records + " Records"] = "";
+  menu[length + " Seconds"] = "";
+  menu["DELETE"] = function() {
+    E.showPrompt("Delete Log " + n).then(ok => {
+      if (ok) {
+        E.showMessage("Erasing...");
+        var f = require("Storage").open(getFileName(n), "r");
+        f.erase();
+        viewLogs();
+      } else viewLog(n);
+    });
+  };
 
-  // Start a background interval to keep recording (adjust interval as needed)
-  recordingInterval = setInterval(() => {
-    // Do nothing, just keep the recording going
-  }, 1000);
+  E.showMenu(menu);
+}
+
+function getFileName(n) {
+  return "accellog." + n + ".csv";
 }
 
 Bangle.loadWidgets();
