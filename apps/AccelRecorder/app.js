8cff16944466a54a5fb7a1c79ea98d92a1ac91f3
atikkhannilgar <63293826+atikkhannilgar@users.noreply.github.com>
2023-11-28 14:51:47 +0100

    Update app.js

--- apps/AccelRecorder/app.js
+++ apps/AccelRecorder/app.js
@@ -165,7 +165,7 @@ function startRecord(force) {
     }
   }
 
-  Bangle.setPollInterval(80000);
+  Bangle.setPollInterval(80);
   Bangle.on('accel', accelHandler);
   recording = true; // Update recording state
 
